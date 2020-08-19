63c893d064e145546fd541e491dc078b0df0eb5a
Jotapelc <joao.paulo.c@hotmail.com>
2020-08-19 01:32:31 -0300

    Iniciando Projeto e carregando para o git

---
+++ ExercicoSantanderJava/src/br/com/santanderExercicio01/Cliente.java
@@ -0,0 +1,13 @@
+package br.com.santanderExercicio01;
+
+public class Cliente {
+
+	private String clienteNumero;
+	private String clienteNome;
+	private String clienteSobrenome;
+	private String clienteRg;
+	private String clienteCpf;
+
+	
+
+}
